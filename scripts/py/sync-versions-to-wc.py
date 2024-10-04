999ecfbeb409c583801a54cf5d3008397f963db7
Alexander Reichert <alex@flecs.tech>
2024-10-04 13:41:57 +0200

    Don't fail version sync if product does not exist yet

--- scripts/py/sync-versions-to-wc.py
+++ scripts/py/sync-versions-to-wc.py
@@ -178,8 +178,11 @@ def main(argv):
         )
     )
     if len(apps) == 0:
-        print("Found no product matching App {}".format(args.app))
-        exit(1)
+        print(
+            "Warning: Found no product matching App {}".format(args.app),
+            file=sys.stderr,
+        )
+        exit(0)
 
     print("ids:\n\t{}".format("\n\t".join(str(app["id"]) for app in apps)))
 
