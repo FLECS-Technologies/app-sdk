d6fab81b708b6c3c37ad356e33c3cc2738717fd0
Alexander Reichert <alex@flecs.tech>
2025-01-27 15:24:16 +0100

    Merge docker-compose into Manifest on build, if exists

--- scripts/build-app-manifest.sh
+++ scripts/build-app-manifest.sh
@@ -9,3 +9,10 @@ LOCAL_APP_MANIFEST="${BUILD_CONTEXT}/manifest.${VARIANT:+${VARIANT}.}json"
 run cp "${LOCAL_APP_MANIFEST}" "${APP_MANIFEST}"
 
 run sed -i "s/##VERSION##/${VERSION}/g" "${APP_MANIFEST}"
+
+if [ -f "${BUILD_CONTEXT}/docker/docker-compose.yml" ]; then
+    echo "Merging docker-compose.yml into manifest"
+    COMPOSE_YAML=$(cat ${BUILD_CONTEXT}/docker/docker-compose.yml | yq -c)
+    run jq ".deployment.compose |= . + ${COMPOSE_YAML}" "${APP_MANIFEST}" >${APP_MANIFEST}.merged
+    run mv -f ${APP_MANIFEST}.merged ${APP_MANIFEST}
+fi
