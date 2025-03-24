a36b159adf4f9dd0908b0946124474f7799c5f1e
Alexander Reichert <alex@flecs.tech>
2025-03-24 16:04:59 +0100

    Login to private registry before cloning compose App images

--- scripts/clone-docker-images.sh
+++ scripts/clone-docker-images.sh
@@ -15,10 +15,18 @@ IMAGES=$(cat ${APP_MANIFEST} | jq -rc ".deployment.compose.yaml.services[].image
 run docker login --username ${DOCKER_USER} --password ${DOCKER_PASSWORD} flecs.azurecr.io >/dev/null
 
 while read IMAGE; do
+    # Parse part before '/': some-registry.example.com/image:tag -> some-registry.example.com
+    REGISTRY_URL=$(echo ${IMAGE} | sed -nE 's#(.*)(/.+)$#\1#p')
+    if [ -n "${PRIVATE_REGISTRY_USER}" ]; then
+        run docker login --username ${PRIVATE_REGISTRY_USER} --password ${PRIVATE_REGISTRY_PASSWORD} ${REGISTRY_URL}
+    fi
     run docker pull ${IMAGE}
-    # Remove the leading part until '/': some-registry.example.com/image:tag -> image:tag
+    # Parse part after '/': some-registry.example.com/image:tag -> image:tag
     BASE_IMAGE=$(echo ${IMAGE} | sed -e 's#^[^/]*/##')
     NEW_TAG="flecs.azurecr.io/${APP}${SUFFIX}/${BASE_IMAGE}"
     run docker tag ${IMAGE} ${NEW_TAG}
     run docker push ${NEW_TAG}
+    if [ -n "${PRIVATE_REGISTRY_USER}" ]; then
+        run docker logout ${REGISTRY_URL}
+    fi
 done < <(cat "${APP_MANIFEST}" | jq -rc ".deployment.compose.yaml.services[].image")
